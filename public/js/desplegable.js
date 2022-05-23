4116a15be7fb557e959908c566b0c9becbf7ea2b
Marina González Suárez <yelose85@gmail.com>
2022-05-23 20:54:11 +0200

    Made some changes to make the categories menu working better

--- public/js/desplegable.js
+++ public/js/desplegable.js
@@ -1,18 +1,10 @@
 
 function mostrarCategoriasDesplegable(){
     const activar = document.querySelector(".desplegable-categorias");
-    activar.style.display = "flex"
+    activar.style.display= "flex"
 }
 function ocultarCategoriasDesplegable(){
     const ocultar = document.querySelector(".desplegable-categorias");
-    ocultar.style.display = "none"
+    ocultar.style.display= "none"
 }
-function mostrar(clase) {
-    debugger
-    const activar = document.querySelector(clase);
-    activar.style.display ="flex"
-}
-function ocultar(clase) {
-    const ocultar = document.querySelector(`.${clase}`);
-    ocultar.style.display ="none"
-}
+console.log("¿estás ahí? JS")
