bf95a14302b0e96d480d272b36a452057d756fb2
Nerea <fernandezcuestanerea@gmail.com>
2022-05-23 14:12:09 +0200

    like color

--- product.js/product.js
+++ product.js/product.js
@@ -3,3 +3,15 @@ let principalimagen = document.getElementById("principal-imagen-product");
 function changeImage (imagen) {
    principalimagen.src = imagen.src;
 }
+
+
+let likeimagen = document.getElementById("like");
+
+function changeImageLike (imagen) {
+    
+    if (likeimagen.src = imagen.src) {
+        likeimagen.src = "/images/like-icon-0pink.png"
+    }
+
+}
+
