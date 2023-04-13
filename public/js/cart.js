51fbcd03cbab157b9c2dc271e370d19b9d52890e
yelose <yelose85@gmail.com>
2023-04-13 14:02:28 +0200

    fixed floating menu and added hover to menu buttons

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,26 +1,26 @@
 let cartQuantity = 0;
-let addItemToCart = function() {
-    cartQuantity ++
+let addItemToCart = function () {
+  cartQuantity++;
+  document.querySelector(".cart-items-quantity").innerHTML = cartQuantity;
+};
+let substractItemFromCart = function () {
+  if (cartQuantity > 0) {
+    cartQuantity--;
     document.querySelector(".cart-items-quantity").innerHTML = cartQuantity;
-}
-let substractItemFromCart = function() {
-    cartQuantity --
-    document.querySelector(".cart-items-quantity").innerHTML = cartQuantity;
-}
+  }
+};
 
 let pluses = document.querySelectorAll(".plus-add-to-cart");
-console.log (pluses);
 
 for (let i = 0; i < pluses.length; i++) {
-    const plus = pluses[i]
-    plus.addEventListener("click", addItemToCart)
+  const plus = pluses[i];
+  plus.addEventListener("click", addItemToCart);
 }
 
 let minus = document.querySelectorAll(".minus-substract-from-cart");
-console.log (minus);
+console.log(minus);
 
 for (let i = 0; i < minus.length; i++) {
-    const substract = minus[i];
-    substract.addEventListener("click", substractItemFromCart)
-    
+  const substract = minus[i];
+  substract.addEventListener("click", substractItemFromCart);
 }
